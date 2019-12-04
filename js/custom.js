16d7cd46e93fece247952a3680f5965f007104f2
ashrafulash <ashraful.freelance@gmail.com>
2019-12-04 21:38:15 +0600

    home page finished without responsive

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,46 @@
 
 /*=====================================
+        Home Page Animatio
+====================================== */
+(function(){ 
+  $(".banner_img").animate({
+    'opacity':1,
+ }, 2000,
+  function(){
+ 
+   $(".home_line").animate({
+     'opacity':1,
+   }, 
+   function () { 
+     $(".banner_left_link").animate({
+      'opacity':1,
+     }, 
+       function(){
+         $(".banner_right_link").animate({
+           'opacity':1,
+           'right':0,
+           'top':'45%',
+          }, 
+            function () {
+                 $(".banner_main_text").animate({
+                   'opacity':1,
+                 })
+             }
+          )
+ 
+       }
+     );
+ })
+ 
+ })
+ 
+ 
+ 
+
+ })();
+
+
+/*=====================================
             Event Handler
 ====================================== */
 
@@ -37,4 +78,4 @@ toggleEvent($("#menu_btn"), $(".menu_list"))
 
    
 
-})()
+})();
